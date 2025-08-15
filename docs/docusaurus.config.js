e567f89122ba673f3e246000079fdc72ebf0bca6
Anirudh Singh <anirudhisgreat1200@gmail.com>
2025-08-15 10:39:47 +0530

    Update docasaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://flutterninja9.github.io',
+  url: 'https://polly.anirudhsingh.in',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/polly_dart/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
